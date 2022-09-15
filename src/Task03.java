180e9c6639a3661b24fca1a50085c39d0a11e3c0
AntonSvirid <adsvirid@tut.by>
2022-09-15 10:16:14 +0300

    did reFactoring of Task03 with static method (вывод положительных чётных чисел)

--- src/Task03.java
+++ src/Task03.java
@@ -1,2 +1,22 @@
 public class Task03 {
+
+    public static String getAllEvenNumbers(int number) {
+        String msg = "";
+        for (int i = 1; i <= number; i++) {
+            if (i % 2 == 1) {
+                continue;
+            }
+            msg += i + " ";
+        }
+        return msg;
+    }
+
+    public static void main(String[] args) {
+
+        int number = Task02.inputPositivNumber();
+
+        String result = getAllEvenNumbers(number);
+
+        System.out.println(result);
+    }
 }
