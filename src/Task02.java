c360f54d03dad204ad4334f1c95d84fd63e37287
AntonSvirid <adsvirid@tut.by>
2022-09-15 10:01:32 +0300

    did reFactoring of Task02 with static method (вывод положительных чисел)

--- src/Task02.java
+++ src/Task02.java
@@ -1,13 +1,33 @@
-// вывод таблицы пифагора
-public class Task02 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
-        for (int count = 1; count < 10; count++) {
-            int n = 10;
-            for (int d = 1; d < n; d++) {
-                System.out.print(d * count + "\t");
+// вывод положительных чисел
+public class Task02 {
+
+    public static int inputPositivNumber() {
+        Scanner scanner = new Scanner(System.in);
+        int number;
+
+        while (true) {
+            System.out.print("Input positive number ");
+            number = scanner.nextInt();
+            if (number > 0) {
+                break;
             }
-            System.out.println();
         }
+        return number;
+    }
+
+    public static void main(String[] args) {
+        int number = inputPositivNumber();
+
+        System.out.println(number);
     }
 }
+
+
+//        for (int count = 1; count < 10; count++) {
+//        int n = 10;
+//        for (int d = 1; d < n; d++) {
+//        System.out.print(d * count + "\t");
+//        }
+//        System.out.println();
