bf3c18f78e16026909fda5e720a51ae2546099ba
AntonSvirid <adsvirid@tut.by>
2022-09-15 10:35:10 +0300

    did reFactoring of Task06 with static method (вывод таблицы пифагора)

--- src/Task05.java
+++ src/Task05.java
@@ -1,2 +1,6 @@
 public class Task05 {
+    public static void main(String[] args) {
+        int number = 50;
+
+    }
 }
--- src/Task06.java
+++ src/Task06.java
@@ -1,2 +1,25 @@
+// PythagorasTable
+
 public class Task06 {
+
+    public static String getPythagorasTable(int dimension) {
+        String msg = "";
+
+        for (int j = 1; j <= dimension; j++) {
+
+            for (int i = 1; i <= dimension; i++) {
+                msg += i * j + "\t";
+            }
+            msg += "\n";
+        }
+        return msg;
+    }
+
+    public static void main(String[] args) {
+        int dimension = Task02.inputPositivNumber();
+
+        String result = getPythagorasTable(dimension);
+
+        System.out.println(result);
+    }
 }
